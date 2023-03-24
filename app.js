764d9d82a5bff350ae62cb7e118c74958fe2d13b
samuelcleo <cleophassamuel@gmail.com>
2023-03-24 13:38:47 +1100

    Used Fetch Implementation & Catching Errors
    - Retreive process.argv inputs as array
    - Make request based on query from command line
    - Throw response errors
    - Try / Catch parsing errors
    - .catch errors for promise rejection

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
-const http = require('http');
-
 function getDefinition(word) {
-    http.get('')
+    fetch(`https://www.dictionaryapi.com/api/v3/references/sd3/json/${word}?key=f27ebc80-0063-46c0-9293-0d4b79564210`)
+    .then( response => {
+        if (response.ok) {
+            return response;
+        } else {
+            throw new Error(response.status)
+        }
+    })
+    .then(response => {
+        try {
+            return response.json();
+        } catch (er) {
+            console.error(error.message);
+        }
+    })
+    .then( json => json[0].shortdef)
+    .then( def => console.dir(def[0]))
+    .catch(error => console.error(error.message))
 }
+
+const words = process.argv.slice(2);
+
+words.forEach(word => getDefinition(word))
